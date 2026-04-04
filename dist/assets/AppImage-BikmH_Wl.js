4b561649da0a2f43af1617895cee36bf5190f28f
JinxX404 <moatazmohammed2392003@gmail.com>
2026-04-04 19:15:23 +0200

    chore: add dist/ to .gitignore, remove build artifacts from tracking

--- dist/assets/AppImage-BikmH_Wl.js
+++
@@ -1,2 +0,0 @@
-import{j as s}from"./index-DV7Dh0M4.js";import"./react-vendor-C-kgDQjY.js";function n({src:r,alt:a="Image Name",className:e="",loading:t="lazy",...m}){return s.jsx("img",{src:r,alt:a,className:e,onError:o=>{o.target.src="/assets/images/no_image.png"},...m})}export{n as I};
-//# sourceMappingURL=AppImage-BikmH_Wl.js.map
